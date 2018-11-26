1163473f1f4dddb17fb62897f27be9d3038b9e17
usmanjaan <usmanshahid@live.ie>
2018-11-26 19:50:48 +0000

    change in button

--- GitHubExample/src/main/java/ie/usman/MyUI.java
+++ GitHubExample/src/main/java/ie/usman/MyUI.java
@@ -29,7 +29,7 @@ public class MyUI extends UI {
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
+        Button button = new Button("Azure");
         button.addClickListener(e -> {
             layout.addComponent(new Label("Thanks " + name.getValue() 
                     + ", it works!"));
